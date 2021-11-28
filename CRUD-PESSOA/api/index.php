15b273f0cdc03a91bafebcfdbddb0be96eb6f67f
luizbaruer <luizbarueri@gmail.com>
2021-11-28 11:52:02 -0300

    atualizado pelo vscode

---
+++ CRUD-PESSOA/api/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso_PHP_Vercel</title>
+    </head>
+    <body>
+        <?php
+            // put your code here
+        ?>    
+            <h1 style = "color: blue;">Curso PHP Vercel</h1>
+        <?php
+        ?>
+    </body>
+</html>
